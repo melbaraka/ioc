52fc076286cfa44b87fcbdbbfa0e6181ae58cb70
melbaraka <m_el-baraka@etu.enset-media.ac.ma>
2024-02-25 00:18:47 +0100

    first commit

---
+++ src/main/java/ext/DaoImpl2.java
@@ -0,0 +1,16 @@
+package ext;
+
+
+import dao.IDao;
+import org.springframework.stereotype.Component;
+
+@Component("dao2")
+public class DaoImpl2 implements IDao {
+
+    @Override
+    public double getData() {
+        System.out.println("version web service");
+        double t = 77;
+        return t;
+    }
+}
